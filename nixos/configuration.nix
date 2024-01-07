75f66cd8bdcb2a0630426a2c3219261531e54db0
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-06 19:45:39 -0500

    reorganize gui config in main nix config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -105,17 +105,15 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # GUI
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
   services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+    # Configure keymap in X11
     layout = "us";
     xkbVariant = "";
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
   };
 
   # Enable CUPS to print documents.
